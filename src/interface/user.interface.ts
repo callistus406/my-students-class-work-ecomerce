524cbb133dc0f36db26e87179b7b988a113d0320
egwaj <egwajohnson.a@gmail.com>
2025-08-22 12:17:05 +0100

    transaction model

---
+++ src/interface/user.interface.ts
@@ -0,0 +1,21 @@
+
+export interface addUser{
+    title:string;
+    firstName:string;
+    lastName:string;
+    gender:string;
+    dateOfBirth:Date;
+    email:string;
+    password:string;
+    phoneNumber:string;
+    address:string;
+    city:string;
+    state:string;
+    country:string;
+    postalCode:string;
+    occupation:string;
+    profileImage:string; 
+    accountNumber:string;
+
+
+}
