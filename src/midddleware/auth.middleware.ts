14c6bcd6cf52d000e4591b96f3a9eba9a17cb6d0
callistus <callistus406@gmail.com>
2025-09-04 22:02:34 +0100

    fea:tupdate

--- src/midddleware/auth.middleware.ts
+++ src/midddleware/auth.middleware.ts
@@ -1,19 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-import { Request,Response, NextFunction } from "express"
-import jwt from 'jsonwebtoken'
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  const authHeader = req.headers.authorization;
+  const jwtSecret = process.env.JWT_ADMIN_KEY as string;
+  const token = authHeader?.split("Bearer ")[1];
+  if (!token) return res.sendStatus(401);
 
-
-export const authMiddleware = (req:Request, res:Response, next:NextFunction):any => {
-    const authHeader = req.headers.authorization;
-    const jwtSecret = process.env.JWT_ADMIN_KEY as string;
-    const token = authHeader?.split("Bearer ")[1]
-    if(!token) return res.sendStatus(401)
-    
-        jwt.verify(token, jwtSecret, (err, data) =>{
-            if(err){
-                return res.sendStatus(401)
-            }
-            next();
-        }) 
+  jwt.verify(token, jwtSecret, (err, data) => {
+    if (err) {
+      return res.sendStatus(401);
     }
-      
+
+    // store the user in the request body
+    next();
+  });
+};
