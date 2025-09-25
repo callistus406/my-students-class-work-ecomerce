5e3a57650e35d2a2809fa1fc15727912d7f0efb7
Tech-bro-code <adigunsodiq27@gmail.com>
2025-09-25 10:07:33 +0100

    loginConfirmation

--- src/utils/login-confirmation-template.ts
+++ src/utils/login-confirmation-template.ts
@@ -1,13 +1,41 @@
-export const confirmationTemplate = (data: { name: string; otp: string }) => {
+export const confirmationTemplate = (data: {
+  name: string;
+  ipAddress: string;
+  userAgent: string;
+}) => {
   return `
-        <html>
-        
-        <body>
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            padding: 20px;
+          }
+          button{
+          background-color: blue;
+          color: white;
+          padding: 10px;
+          pointer:cursor;
 
-            <h2> HELLO: ${data.name}: </h2>
-            <br />
-            <h2> THIS IS YOU OTP: ${data.otp} </h2>
-        </body>
-        <html>
+          }
+          .container {
+            background-color: #ffffff;
+            padding: 30px;
+            border-radius: 8px;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            max-width: 600px;
+            margin: auto;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h2>Hello, ${data.name}</h2>
+          <p>Someone just logged into your account near ${data.ipAddress} on ${data.userAgent}. if this wasn't you , we're here to help you take some steps to secure account</p>
+          <button>This wasn't me</button>
+        </div>
+      </body>
+    </html>
         `;
 };
