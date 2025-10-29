3acbb146e80affb2b928e53ab31f78ba864f12c1
egwaj <egwajohnson.a@gmail.com>
2025-10-29 15:03:48 +0100

    feat: check stock

--- src/services/cart.services.ts
+++ src/services/cart.services.ts
@@ -26,13 +26,13 @@ export class cartService {
     // Calculate total price
     const price = product.discountPrice ?? product.price;
 
-    //check if product is avauilable
     // check if product is in stock
-    //
+    const stock =  await productRepository.findById(data.quantity as any);
+    if(!stock) throw throwCustomError("Product out of stock", 400);
+    //get user cart
     const cart = await cartModel.findOne({ ownerId: userId });
     if (!cart) {
       //create the cart
-
       const res = await cartModel.create({
         ownerId: userId,
         items: [
